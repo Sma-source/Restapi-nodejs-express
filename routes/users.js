b808e7420e2d62c959c775f39cefbaee3a05554c
Sma-source <rabhi.smain@gmail.com>
2020-12-23 14:09:29 +0100

    add users in json object format

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,24 @@ import express from "express";
 // init router
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 29,
+  },
+
+  {
+    firstName: "Max",
+    lastName: "Opera",
+    age: 40,
+  },
+];
+
 // all routes in here are starting with /users
 router.get("/", (req, res) => {
-  res.send("hello");
+  console.log(users);
+  res.send(users);
 });
 
 export default router;
