2c1d1bd4fd1a7c5d692d9280ca610bf48d2bd015
Sma-source <rabhi.smain@gmail.com>
2021-01-01 23:20:19 +0100

    init

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-const express = require("express");
+import express from "express";
+import bodyParser from "body-parser";
+
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.post("/", (req, res, next) => {
+  console.log(req.body);
+  res.json({
+    test: req.body,
+  });
+});
 
 app.listen(3000, () => console.log("Server Started"));
