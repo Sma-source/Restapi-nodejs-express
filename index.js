e8f7421ba014ae0e3bf0f42a186aef4b6582e1c8
Sma-source <rabhi.smain@gmail.com>
2021-01-02 20:18:17 +0100

    add connection to database mongodb

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
+import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
+import moogoose from "mongoose";
 
+dotenv.config();
 const app = express();
-
+moogoose.connect(
+  process.env.DATABASE_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected");
+  }
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
